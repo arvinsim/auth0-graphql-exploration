24d0585177e3ff6b73c54312715d241c97111bcc
Arthur Vincent Simon <developarvin@gmail.com>
2020-02-12 18:21:13 +0800

    Set routes

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Login } from "./pages/Login";
 import { LoginCallback } from "./pages/LoginCallback";
+import { Dashboard } from "./pages/Dashboard";
 
 export function AppRoutes() {
   return (
@@ -13,6 +14,9 @@ export function AppRoutes() {
         </Route>
         <Route path="/login-callback">
           <LoginCallback />
+        </Route>
+        <Route path="/dashboard">
+          <Dashboard />
         </Route>
       </Switch>
     </Router>
--- src/pages/LoginCallback.tsx
+++ src/pages/LoginCallback.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export function LoginCallback() {
-    return (
-        <div>
-            Login Callback
-        </div>
-    );
-};
+  return <div>Login Callback</div>;
+}
