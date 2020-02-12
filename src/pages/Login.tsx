99027b02fd45b6a588e5fb8bda77bec29284100e
Arthur Vincent Simon <developarvin@gmail.com>
2020-02-12 18:04:10 +0800

    Start testing for Auth0 form

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Login(props: any) {
+    return (
+        <div>
+            Login
+        </div>
+    );
+};
---
+++ src/pages/LoginCallback.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginCallback(props: any) {
+    return (
+        <div>
+            Login Callback
+        </div>
+    );
+};
